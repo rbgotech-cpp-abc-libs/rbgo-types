49693002a623af098e731151a5d771945d137f7d
rbgo <rbgo.web@gmail.com>
2023-06-19 09:48:06 +0200

    ADD include/rbgo_types.h
    The header file with definitions of types and symbols;
    what this repo is about.

---
+++ include/rbgo_types.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace rbgo {
+
+typedef	char					Ti8;
+typedef short					Ti16;
+typedef int						Ti0;
+typedef long					Ti32;
+typedef long long				Ti64;
+typedef unsigned char			Tu8;
+typedef unsigned short			Tu16;
+typedef unsigned int			Tu0;
+typedef unsigned long			Tu32;
+typedef unsigned long long		Tu64;
+typedef float					Tf32;
+typedef double					Tf64;
+typedef long double				Tf96;
+
+}//ns
